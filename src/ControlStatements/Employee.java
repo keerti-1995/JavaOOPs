b3cb26bbbfac742bd4e48e1da012ff51a3fdf889
keerti <keerti.herurmath@gmail.com>
2025-04-02 09:48:46 -0500

    Modified Employee class

--- src/ControlStatements/Employee.java
+++ src/ControlStatements/Employee.java
@@ -1,6 +1,21 @@
 package ControlStatements;
 
-public class Employee {
-	public String temp;
+public class Employee
+{
+	public String ename;
+	 public int salary;
+	
+	public void display()
+	{
+		if(salary>50000)
+		{
+			System.out.println("Employee is manager");
+		}
+		else
+		{
+			System.out.println("Employee is associate");
+			
+		}
+	}
 
 }
